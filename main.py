ded0549ffa8dbde177c6716b06a43f2f75137316
Paul Zhou <paulzhousz@gmail.com>
2016-05-23 22:35:31 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+#########################################################
+# File Name: main.py
+# Description: 用于启动scrapy爬虫
+# Author: Paul Zhou(paulzhousz@gmail.com)
+# Created Time:2016-05-23 22：26
+# Update Log:
+#########################################################
+
+from scrapy import cmdline
+
+cmdline.execute("scrapy crawl newohr".split())
