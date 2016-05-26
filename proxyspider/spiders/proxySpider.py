a196fcbfec8dfe46216ecba34f96b2eedd380739
Paul Zhou <paulzhousz@gmail.com>
2016-05-26 17:47:49 +0800

    update

---
+++ proxyspider/spiders/proxySpider.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+====================================================================
+File Name:proxySpider.py
+Description:
+    爬虫主程序，抓取www.xicidaili.com中代理列表
+Author: Paul Zhou(paulzhousz@gmail.com)
+Created Time: 2016-5-25 17:00
+Update Log:
+  1.
+  2.
+=====================================================================
+"""
+
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class ProxySpider(CrawlSpider):
+    pass
