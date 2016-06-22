35541aa79bbb7dc06409c3d5370ba426320f0cd4
Paul Zhou <paulzhousz@gmail.com>
2016-06-22 17:17:20 +0800

    update

--- proxymain.py
+++ proxymain.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 #########################################################
-# File Name: main.py
+# File Name: proxymain.py
 # Description: 用于启动proxy scrapy爬虫
 # Author: Paul Zhou(paulzhousz@gmail.com)
 # Created Time:2016-05-23 22：26
@@ -9,6 +9,11 @@
 #########################################################
 import argparse
 
-from scrapy import cmdline
 
-cmdline.execute("scrapy crawl xici".split())
+if __name__=='__main__':
+
+
+
+# from scrapy import cmdline
+#
+# cmdline.execute("scrapy crawl xici".split())
